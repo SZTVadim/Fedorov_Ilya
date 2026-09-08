415fa833250e50b516a0756069b94cd8ac7bc0e8
Ilya Fedorov <0533608758ilia@gmail.com>
2026-09-08 18:37:08 +0300

    Move homework 15 to separate file

--- lessons/hw15/hw15.py
+++ lessons/hw15/hw15.py
@@ -1,8 +1,39 @@
-def find_log_entries(log_type):
-    with open("data_test/application.log", "r", encoding="utf-8") as file:
-        for line in file:
-            if log_type in line:
-                print(line.strip())
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.__price = 0
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            print("Ошибка: цена не может быть отрицательной!")
+        elif value > 10000:
+            print("Ошибка: максимальная цена 10000 рублей!")
+        else:
+            self.__price = value
+
+    @classmethod
+    def create_from_string(cls, book_string):
+        title, author = book_string.split("|")
+        return cls(title, author)
+
+    def get_info(self):
+        return f"Книга '{self.title}' автор {self.author}, цена {self.price} руб."
 
 
-find_log_entries("ERROR")
+book1 = Book("1984", "Оруэлл")
+book2 = Book.create_from_string("Мастер и Маргарита|Булгаков")
+
+book1.price = 500
+book2.price = 750
+
+book1.price = -100
+book1.price = 15000
+
+print(book1.get_info())
+print(book2.get_info())
